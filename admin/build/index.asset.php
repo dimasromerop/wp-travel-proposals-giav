e125bf1327768847cbeb0ffa508feac3899c139c
dimasromerop <you@example.com>
2026-01-02 00:56:07 +0100

    commit

--- admin/build/index.asset.php
+++ admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '0631f96dadd9921efd0c');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '95494d0c62eac04f2b7e');
