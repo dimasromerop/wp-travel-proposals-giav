968f746f557ffcda47302e4c151a6099071c4754
dimasromerop <you@example.com>
2026-01-09 21:20:06 +0100

    commit

--- admin/build/index.asset.php
+++ admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'bc0f3c778d0f73dfbfd2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '00f6fc64b046937fccde');
--- admin/build/portal.asset.php
+++ admin/build/portal.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '5a11633b3622bc32fc87');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'd100c053b097703a5c1a');
