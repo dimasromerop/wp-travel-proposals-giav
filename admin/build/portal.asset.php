36c1a57d33e4a8afa266467fa4137ace9e133905
dimasromerop <you@example.com>
2026-01-10 09:57:17 +0100

    commit

--- admin/build/portal.asset.php
+++ admin/build/portal.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '946b4ab049040b2b229c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '189c70ec0d2232138313');
