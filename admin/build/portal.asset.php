c9ba126616ff4b1ddddd07298bb0ba0278fd9448
dimasromerop <you@example.com>
2026-01-27 19:48:27 +0100

    commit

--- admin/build/portal.asset.php
+++ admin/build/portal.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f8a1118f6f229a635c4e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '83f69929e3739300cb5b');
--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -1,5 +1,16 @@
 const path = require('path');
 const baseConfig = require('@wordpress/scripts/config/webpack.config');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const plugins = (baseConfig.plugins || []).map((plugin) => {
+  if (plugin && plugin.constructor && plugin.constructor.name === 'MiniCssExtractPlugin') {
+    return new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[name].[contenthash:8].css',
+    });
+  }
+  return plugin;
+});
 
 module.exports = {
   ...baseConfig,
@@ -7,4 +18,10 @@ module.exports = {
     index: path.resolve(__dirname, 'src/index.js'),
     portal: path.resolve(__dirname, 'src/portal/index.js'),
   },
+  output: {
+    ...baseConfig.output,
+    filename: '[name].js',
+    chunkFilename: '[name].[contenthash:8].js',
+  },
+  plugins,
 };
